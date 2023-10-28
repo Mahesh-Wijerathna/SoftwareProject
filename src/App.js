e5ac0fb6a9ea5133c00df600f6657888af005519
Mahesh_Wijerathna <maheshtharanga49@gmail.com>
2023-10-28 14:32:36 +0530

    change_by_me

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          All things fine
+          Edit  and saved still not still image cannot be removed.
         </p>
         <a
           className="App-link"
