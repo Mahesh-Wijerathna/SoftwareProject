35dd2f7673ef4dfb258cd222d0cc3f94b4024c2c
Mahesh_Wijerathna <maheshtharanga49@gmail.com>
2023-12-06 14:12:50 +0530

    changes_by_ma

---
+++ src/Pages/LoginPage.jsx
@@ -0,0 +1,25 @@
+
+import '../App.css';
+
+function LoginPage() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          login page is here <br/>
+          
+
+        </p>
+        <a
+          
+        >
+          හදාගෙන යන්න ඕන 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default LoginPage;
